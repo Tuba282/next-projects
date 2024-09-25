d08e9a51c19a5b65ae9d46b6b041203d6bb8732c
Tuba282 <tubajan282@gmail.com>
2024-09-25 22:56:53 +0500

    next-04-practise

--- practise-04-next/app/components/footer/page.tsx
+++ practise-04-next/app/components/footer/page.tsx
@@ -1,11 +1,34 @@
+'use client'
 import Link from "next/link";
+import { useState,useEffect} from "react";
 
 export default function Footer(){
+    
+  const [data, Setdata] = useState(0)
+
+    //   useEffect(()=>{},[]) useEffect ek function  contains two arguments (1.callback function)(2. array)
+    useEffect(()=>{
+        console.log('every time');
+        
+    }) 
+    useEffect(()=>{
+        console.log('first time when site reload');
+        
+    },[]) 
+    useEffect(()=>{
+        console.log('run every time when update/changes in dependency');
+        
+    },[data]) 
+
     return (
         <>
         <h1 className="text-8xl bg-red-400 text-white text-center">This is Footer</h1>
         <Link href={'/'} ><h1 className="text-8xl bg-red-300 text-black text-center">Home</h1></Link>
-
+        <div className="flex justify-around items-center px-32 mt-4">
+        <p onClick={() => Setdata(data + 1)} className="border-2 border-red-600 px-3 py-2 bg-red-300 cursor-pointer">+</p>
+        <h1 className="border-2 border-red-600 px-3 py-2 bg-red-300">{data}</h1>
+        <p onClick={() => Setdata(data - 1)} className="border-2 border-red-600 px-3 py-2 bg-red-300 cursor-pointer">-</p>
+      </div>
 
         </>
     )
--- practise-04-next/app/list.md
+++ practise-04-next/app/list.md
@@ -8,5 +8,5 @@ navigation  (2 types/ways)
     1.useRouter(); 'kisi variable me store kar k push kardo' "onclick"  ✅
     2.useRoute -->function 'jaga jaga call karty raho' ✅
 props
-usestate {variable (changable) 'abi iski value 2 hai but infuture change hosakti hai'}
-useEffect
+usestate ✅ {variable (changable) 'abi iski value 2 hai but infuture change hosakti hai'}
+useEffect ✅ {mostly used when we have to fetch api} what is api -->  server------bridge-----user
