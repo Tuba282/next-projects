10faaa3dd4b51d78589daae59743aab5ae6115b3
Tuba282 <tubajan282@gmail.com>
2024-10-13 22:38:45 +0500

    useState , useEffect , useRef

--- practise-05-next/src/components/useRef.tsx
+++ practise-05-next/src/components/useRef.tsx
@@ -1,20 +1,51 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function TextInput() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [text, setText] = useState<string>()
 
   const focusInput = () => {
     if (inputRef.current) {
+      setText(inputRef.current.value)
       inputRef.current.focus();
     }
   };
-
+  
   return (
     <div>
       <input ref={inputRef} type="text" />
-      <button onClick={focusInput}>Focus Input</button>
+      <p>{text}</p>
+      <button onClick={focusInput}>Show Input</button>
     </div>
   );
 }
 
 export default TextInput;
+
+
+// import { useRef, useState } from 'react';
+
+// const MyComponent = () => {
+//   const countRef = useRef(0);
+//   const [count, setCount] = useState(0);
+
+//   const incrementCount = () => {
+//     countRef.current += 1;
+//     setCount(countRef.current); // Update state to trigger re-render
+//   };
+
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           countRef.current -= 1;
+//           setCount(countRef.current)
+//         }}
+//       >Deccrement Count</button>
+//       <p>Count: {count}</p>
+//       <button onClick={incrementCount}>Increment Count</button>
+//     </div>
+//   );
+// };
+
+// export default MyComponent;
